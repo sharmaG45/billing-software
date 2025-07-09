a8f1c5e05831e9544c1af2bc6ec3c42855f83809
shubhamG45 <shubhamhjp92@gmail.com>
2025-07-09 12:11:28 +0530

    Fresh start without large files-1

--- renderer/pages/_document.js
+++ renderer/pages/_document.js
@@ -1,17 +1,17 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
-  render() {
-    return (
-      <Html>
-        <Head>
-          {/* Optional: custom fonts, favicon, meta */}
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+    render() {
+        return (
+            <Html>
+                <Head>
+                    {/* Custom <meta>, fonts, favicon can go here */}
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
 }
