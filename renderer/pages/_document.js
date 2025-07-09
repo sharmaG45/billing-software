14ceec6ea429655c7ccaea2689063908bb3cffec
shubhamG45 <shubhamhjp92@gmail.com>
2025-07-09 11:53:50 +0530

    Fresh start without large files-1

--- renderer/pages/_document.js
+++ renderer/pages/_document.js
@@ -1,17 +1,11 @@
-// renderer/pages/_document.js
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
-  }
-
   render() {
     return (
       <Html>
         <Head>
-          {/* custom <meta> tags, fonts, icons etc */}
+          {/* Optional: custom fonts, favicon, meta */}
         </Head>
         <body>
           <Main />
